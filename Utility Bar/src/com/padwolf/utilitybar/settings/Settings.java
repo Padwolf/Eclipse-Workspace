52b7d0ec83c73e0154e94509f77140010399c379
Patrick  Duane <970098955@aps.edu>
2016-05-01 16:57:58 -0600

    battery dying

--- Utility Bar/src/com/padwolf/utilitybar/settings/Settings.java
+++ Utility Bar/src/com/padwolf/utilitybar/settings/Settings.java
@@ -1,11 +1,28 @@
 package com.padwolf.utilitybar.settings;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 
 public class Settings {
 	
 	public Settings(){
-		System.out.println(new File("settings.sdt)").getAbsolutePath());
+		File file = new File("settings.sdt");
+		try {
+			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+			InputStream in = new BufferedInputStream(new FileInputStream(file));
+			if (!file.exists()){
+				file.createNewFile();
+				
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
